2a2afd349d1347a037e9bd4c72b66b33a2803d09
Alessandro Ferraro <ale_ferraro@libero.it>
2021-03-21 15:39:32 +0100

    refactoring

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 /*
+Ciao ragazzi,
+Esercizio di oggi: Slider
+Cartella/Repo: js-jq-carousel
+Descrizione:
 Creare uno slider di immagini
 Potete usare le immagini che desiderate.
 Per facilitarvi la vita usate immagini delle stesse dimensioni :)
@@ -6,7 +10,15 @@ Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagi
 Per scorrere le immagini permettere anche l'uso delle frecce sinistra e destra della tastiera ( e' un evento diverso dal click, quale? )
 Utiliziamo una classe first e last  per capire quali sono la prima e ultima immagine dello slider
 Utilizziamo una classe active per aiutarci a capire quale è l'immagine attuale da visualizzare nello slider
+Bonus:
+Clicchiamo sui pallini e mostriamo l'immagine corrispondente
+Generiamo i pallini con JS
+Consiglio del giorno:
+Come sempre è importante usare la documentazione di jQuery per scoprire/rinfrescare memoria sulle funzioni che potrebbero esserci utili.
+Se volete potete partire dal layout base visto in classe
+https://bitbucket.org/booleancareers/lc-ex-slider-layout/downloads/
 */
+
 
 //al click della freccia 'prev'
 $('.prev').click(function(){
@@ -49,6 +61,30 @@ $('.next').click(function(){
 
 /*
 Bonus:
-Clicchiamo sui pallini e mostriamo l'immagine corrispondente
 Generiamo i pallini con JS
 */
+
+/*
+Bonus:
+Clicchiamo sui pallini e mostriamo l'immagine corrispondente
+*/
+
+$('.nav > i').click(function(){
+  //imposto la class active al pallino cliccato
+  $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+
+  var selectedDot = $(this).index();
+  console.log(selectedDot);
+
+  var images = $(this).parent().siblings(".images").children();
+  console.log(images);
+
+  for(var i=0; i < images.length; i++){
+    if(selectedDot == i){
+      $(images[i]).addClass('active');
+    } else {
+      $(images[i]).removeClass('active');
+    }
+  }
+})
